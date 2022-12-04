7a3f81789486ed8d763a23ab59204b342bffa0bb
AAA1exa8 <alexandrpleskot@gmail.com>
2022-12-04 11:37:15 +0100

    Day 4

--- Day4/Day4A.py
+++ Day4/Day4A.py
@@ -1,13 +1,12 @@
-text = open('./input.txt', 'r')
+text = open('./input2.txt', 'r')
 
 array = []
 repeats = 0
 for lines in text:
     tmp1 = lines.split(',')
+    #array.append(items.replace('\n', '').split('-') for items in tmp1)
     for items in tmp1:
-        items = items.replace('\n', '')
-        tmp2 = items.split('-')
-        array.append(tmp2)
+        array.append(items.replace('\n', '').split('-'))
     array1 = set(range(int(array[0][0]), int(array[0][1])+1))
     array2 = set(range(int(array[1][0]), int(array[1][1])+1))
     if ((array1 & array2) == array1) or ((array1 & array2) == array2):
