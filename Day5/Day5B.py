53fe8785f510a234efc310dd07f9648eead664b1
AAA1exa8 <alexandrpleskot@gmail.com>
2022-12-05 15:13:08 +0100

    day 5

---
+++ Day5/Day5B.py
@@ -0,0 +1,32 @@
+commands = open('./input.txt', 'r')
+
+cargo = [['Z', 'N'], ['M', 'C', 'D'],['P']]
+cargo2 = [['P', 'F', 'M', 'Q', 'W', 'G', 'R', 'T'], 
+         ['H', 'F', 'R'], 
+         ['P', 'Z', 'R', 'V', 'G', 'H', 'S', 'D'],
+         ['Q', 'H', 'P', 'B', 'F', 'W', 'G'],
+         ['P', 'S', 'M', 'J', 'H'],
+         ['M', 'Z', 'T', 'H', 'S', 'R', 'P', 'L'],
+         ['P', 'T', 'H', 'N', 'M', 'L'],
+         ['F', 'D', 'Q', 'R'],
+         ['D', 'S', 'C', 'N', 'L', 'P', 'H']]
+
+skip = False
+for command in commands:
+    if command != '\n' and not skip:
+        continue
+    elif command == '\n':
+        skip = True
+        continue
+    command = command.replace('\n', '').split(' ')
+    howMuch = int(command[1])
+    fromWhere = int(command[3])
+    toWhere = int(command[5])
+    position = len(cargo2[fromWhere - 1]) - 1
+    for i in range(howMuch):
+        cargo2[toWhere - 1].append(cargo2[fromWhere - 1][-(howMuch - i)])
+        cargo2[fromWhere - 1].pop(-(howMuch - i))
+        
+print(cargo2)
+for char in cargo2:
+    print(char[-1:][0], end='')
